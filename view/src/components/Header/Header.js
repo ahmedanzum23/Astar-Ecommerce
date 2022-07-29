f8ebe1e2b30072d1f10b6663b9609b78213e5e10
ahmedanzum23 <ahmedanzum23@gmail.com>
2022-07-30 01:27:33 +0600

    second

---
+++ view/src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css';
+
+
+const Header = () => {
+    return (
+<nav className='header'>
+        <img className="logo" src={logo} alt="" />
+
+        <div>
+        <a href="/shop">Shop</a>
+                <a href="/orders">Order Review</a>
+                <a href="/inventory">Manage Inventory</a>
+                <a href="/about">About</a>
+        </div>
+
+        </nav>
+    );
+
+
+};
+
+export default Header;
+
